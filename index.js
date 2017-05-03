e12b4a405f0b1390273d0b016dbd6bc14fef1606
MQuy <sugiacupit@gmail.com>
2017-05-04 01:01:04 +0800

    Append cond assets

--- index.js
+++ index.js
@@ -1,49 +1,27 @@
-/**
- * @license
- * Copyright 2017 Google Inc.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 'use strict';
 
 const objectAssign = require('object-assign');
-const defaultOptions = {
-  rel: 'preload',
-  as: 'script',
-  include: 'asyncChunks',
-  fileBlacklist: [/\.map/]
-};
 
-class PreloadPlugin {
+class HtmlWebpackConditionAsset {
   constructor(options) {
     this.options = objectAssign({}, defaultOptions, options);
   }
 
   apply(compiler) {
     const options = this.options;
-    let filesToInclude = '';
-    let extractedChunks = [];
+
     compiler.plugin('compilation', compilation => {
       compilation.plugin('html-webpack-plugin-alter-asset-tags', (htmlPluginData, cb) => {
-        const template = `
-          scripts = [${htmlPluginData.body.map(tag => `'${tag.attributes.src}'`)}];
-          scripts.forEach(function(src) {
-            var scriptEl = document.createElement('script');
-            scriptEl.src = src;
-            scriptEl.async = false;
-            document.head.appendChild(scriptEl);
-          });
-        `
+        const publicPath = compilation.outputOptions.publicPath || '';
+        const fixedTags = this.filterTags(compilation, 'fixed');
+        const condTags = this.filterTags(compilation, 'cond');
+        const condAssets = condTags.map((tag) => {
+          const asset = this.options.assets.find((asset) => asset.chunkName == tag.chunkName);
+          tag.condition = asset.condition;
+          return tag;
+        });
+        const template = buildTemplate(fixedTags, condAssets);
+
         htmlPluginData.body = [{
           tagName: 'script',
           closeTag: true,
@@ -56,6 +34,49 @@ class PreloadPlugin {
       });
     });
   }
+
+  filterTags(compilation, type) {
+    const assetNames = this.options.assets.map((option) => option.chunkName);
+    return compilation
+      .chunks
+      .filter((chunk) => {
+        let isRanged = chunk.isInitial();
+        if (type == 'fixed') {
+          isRanged &= !assetNames.includes(chunk.name);
+        } else if (type == 'cond') {
+          isRanged &= assetNames.includes(chunk.name);
+        } else {
+          isRanged = false;
+        }
+        return isRanged;
+      })
+      .map((chunk) => {
+        return chunk
+                  .files
+                  .filter((tag) => /\.js$/.test(tag))
+                  .map((file) => ({ chunkName: chunk.name, src: file }));
+      })
+      .reduce((prev, curr) => prev.concat(curr), [])
+  }
+
+  buildTemplate(fixedTags, condAssets) {
+    return `
+      scripts = [${fixedTags.map(tag => `'${publicPath}${tag.src}'`)}];
+      ${condAssets.map((asset) => {
+        return `
+          if (${asset.condition}) {
+            scripts.unshift('${publicPath}${asset.src}');
+          }
+        `
+      })}
+      scripts.forEach(function(src) {
+        var scriptEl = document.createElement('script');
+        scriptEl.src = src;
+        scriptEl.async = false;
+        document.head.appendChild(scriptEl);
+      });
+    `;
+  }
 }
 
-module.exports = PreloadPlugin;
+module.exports = HtmlWebpackConditionAsset;
